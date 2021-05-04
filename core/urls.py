8974b3b4ffe53be55eb6a2dbc54b7207bd6bd950
HerlesINC <herles.incalla@gmail.com>
2021-05-04 17:06:00 -0500

    Settingup jwt

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('healths.urls', namespace='healths')),
     path('api/', include('healths_api.urls', namespace='healths_api')),
+    path('api/user/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
