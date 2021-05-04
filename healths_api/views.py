bb78ab30aa9f6ccebb492e861cdacc3b18f83089
HerlesINC <herles.incalla@gmail.com>
2021-05-04 14:29:31 -0500

    Add permission_classes: IsAdminUser

--- healths_api/views.py
+++ healths_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
 from healths.models import Health
 from .serializers import HealthSerializer
+from rest_framework.permissions import IsAdminUser
 
 class HealthList(generics.ListCreateAPIView):
+  permission_classes = [IsAdminUser]
   queryset = Health.postobjects.all()
   serializer_class = HealthSerializer
 
