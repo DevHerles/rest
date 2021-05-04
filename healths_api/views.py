5706daf94be3313a49a87d11177620ae20499126
HerlesINC <herles.incalla@gmail.com>
2021-05-04 15:21:31 -0500

    Add custom permissions

--- healths_api/views.py
+++ healths_api/views.py
@@ -1,13 +1,24 @@
 from rest_framework import generics
 from healths.models import Health
 from .serializers import HealthSerializer
-from rest_framework.permissions import IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, DjangoModelPermissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissionsOrAnonReadOnly, DjangoModelPermissions
+
+
+class HealthUserWritePermission(BasePermission):
+  message = 'Editing records is resctricted to the author only.'
+
+  def has_object_permission(self, request, view, obj):
+    if request.method in SAFE_METHODS:
+      return True
+
+    return obj.owner == request.user
 
 class HealthList(generics.ListCreateAPIView):
   permission_classes = [DjangoModelPermissions]
   queryset = Health.postobjects.all()
   serializer_class = HealthSerializer
 
-class HealthDetail(generics.RetrieveDestroyAPIView):
+class HealthDetail(generics.RetrieveUpdateDestroyAPIView, HealthUserWritePermission):
+  permission_classes = [HealthUserWritePermission]
   queryset = Health.objects.all()
   serializer_class = HealthSerializer
